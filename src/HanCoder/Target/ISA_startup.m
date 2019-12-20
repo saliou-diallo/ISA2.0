d2a73c9900723fa0541ca536f0ceecf82c8c6ef9
Saliou Diallo <saliou.diallo@hotmail.com>
2019-12-20 09:30:28 +0100

    ISA2.0 tested on 19/12/2019. Works as it should. Only addition needed is a physical on and of button, and beginning of motorway recognition.

--- src/HanCoder/Target/ISA_startup.m
+++ src/HanCoder/Target/ISA_startup.m
@@ -1,11 +1,7 @@
-%% Input signals
-V_max = 50 % maximum speed in km/h
-V_vec = 1 % Vehicle speed in km/h
+%% Pedal depression
+Pedal_depression_treshold = 3120
 %% Output signals
-PWM_motor = 0.6 % PWM signal.
-%% Defining vehicle messages
-% a = hex2dec (["#""#""#"])
-%% Defining maximum speed
-% MaxSpeed
-%% Hextest
-Hex_value = uint8('F')
+PWM_motor = 0.55 % PWM signal
+%% Mobileye data
+Value = [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45]
+Speed = [0 1 2 3 4 6 7 9 10 11 13 14 15 17 18 19 21 22 23 25 26 27 29 30 31 33 34 35 37 38 39 41 42 43 45 46 47 49 50 51 53 54 55 57 58 59]
